1c9bcedbf114b0d2f84457f1681be322ae50b79a
Demetrio <dtreebeats@gmail.com>
2026-05-18 13:56:17 -0300

    feat estrutura inicial da interface

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="bg-[#09090B] text-white min-h-screen">
+      <Header />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
